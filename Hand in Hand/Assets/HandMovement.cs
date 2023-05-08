b66a7656c5ce822a775c7bd2e658fd011ac668cf
dewgong5 <129033660+dewgong5@users.noreply.github.com>
2023-05-08 23:00:35 +0700

    movement.1

--- Hand in Hand/Assets/HandMovement.cs
+++ Hand in Hand/Assets/HandMovement.cs
@@ -1,21 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HandMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed;
+    public bool doublejump;
+    public bool hasjumped;
     void Start()
     {
-        
+        hasjumped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            if (hasjumped == false)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+              
+            }
+            hasjumped = true;
+
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (doublejump == false)
+        {
+            hasjumped = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (doublejump == false)
+        {
+            hasjumped = true;
         }
     }
 }
