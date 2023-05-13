858d753378a99f63d1c3a6c012a1ea268f634625
dewgong5 <129033660+dewgong5@users.noreply.github.com>
2023-05-13 17:08:42 +0700

    doublejump

--- Hand in Hand/Assets/HandMovement.cs
+++ Hand in Hand/Assets/HandMovement.cs
@@ -8,6 +8,9 @@ public class HandMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
+    public bool DoubleJumpActive;
+    public float maxJumps;
+    private float JumpsLeft;
     Rigidbody2D rb;
     BoxCollider2D coll;
     [SerializeField] LayerMask JumpableGround;
@@ -17,16 +20,33 @@ public class HandMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        JumpsLeft = maxJumps;
+        maxJumps = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump") && isGrounded())
+        bool JumpButton = Input.GetButtonDown("Jump");
+        float dirX = Input.GetAxisRaw("Horizontal");
+
+        if (JumpButton && JumpsLeft > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, 7);
+            JumpsLeft -= 1;
         }
-        float dirX = Input.GetAxisRaw("Horizontal");
+
+        if (isGrounded() && rb.velocity.y <=0 )
+        {
+            JumpsLeft = maxJumps;
+        }
+
+        if (DoubleJumpActive)
+        {
+            maxJumps = 2;
+        }
+        
+
         rb.velocity = new Vector2 (dirX * speed, rb.velocity.y);
     }
 
